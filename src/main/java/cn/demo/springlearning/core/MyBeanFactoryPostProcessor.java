8f789e683dafdd3c2f35e67beebeddfda8766b9b
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-05-20 23:21:21 +0800

    一些新增

---
+++ src/main/java/cn/demo/springlearning/core/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package cn.demo.springlearning.core;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * Bean工厂后置处理器实现。
+ *
+ * @author Jinhua
+ * @version 1.0
+ * @date 2021/5/20 23:06
+ */
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+}

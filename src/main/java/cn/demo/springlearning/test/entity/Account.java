ed207f47d23c7cd0a696765d44eb1ae3f04572e1
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-06-07 17:20:06 +0800

    指定字段

--- src/main/java/cn/demo/springlearning/test/entity/Account.java
+++ src/main/java/cn/demo/springlearning/test/entity/Account.java
@@ -2,6 +2,7 @@ package cn.demo.springlearning.test.entity;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
  * @date 2021/6/3 23:40
  */
 @Data
+@NoArgsConstructor
 @Builder
 public class Account {
 

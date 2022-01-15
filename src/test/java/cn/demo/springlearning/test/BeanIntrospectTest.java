505572d1ced1b49633f57726d6f228fc1c5a47d5
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-01-15 19:21:46 +0800

    1. Resource体系整理； 2. Bean内省的单测类。

---
+++ src/test/java/cn/demo/springlearning/test/BeanIntrospectTest.java
@@ -0,0 +1,26 @@
+package cn.demo.springlearning.test;
+
+import cn.demo.springlearning.entity.Account;
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+
+/**
+ * JavaBean的内省机制测试
+ *
+ * @author Jinhua-Lee
+ * @version 1.0
+ * @date 2022/1/4 22:13
+ */
+public class BeanIntrospectTest {
+
+    @Test
+    @SneakyThrows
+    public void testBeanIntrospect() {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Account.class, Object.class);
+
+        System.out.println();
+    }
+}

1e935b14e2e19a97cda3be340b5f68e874da72c3
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-03-30 17:38:06 +0800

    1. WebFilter的修正.

--- src/main/java/cn/spring/learning/mvc/config/WebFilterConfig.java
+++ src/main/java/cn/spring/learning/mvc/config/WebFilterConfig.java
@@ -9,15 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import javax.servlet.Filter;
 
 /**
+ * 过滤器顺序测试：<p>&emsp;
+ * 1. 原有WebFilter注解是按类名或filter名排序；<p>&emsp;
+ * 2. 如果在SpringBoot下，必须去除原有注解，通过注册Bean来完成排序。<p>
+ *
  * @author Jinhua
  * @version 1.0
  * @date 2022/3/30 10:33
  */
 @Configuration
-public class FilterOrderConfig {
+public class WebFilterConfig {
 
     @Bean
-    public FilterRegistrationBean<Filter> sessionFilter() {
+    public FilterRegistrationBean<Filter> sessionFilterBean() {
         FilterRegistrationBean<Filter> sessionFilterBean = new FilterRegistrationBean<>();
         sessionFilterBean.setFilter(new SessionFilter());
         sessionFilterBean.setOrder(1);
@@ -33,4 +37,5 @@ public class FilterOrderConfig {
         encodingFilterBean.addUrlPatterns("/*");
         return encodingFilterBean;
     }
+
 }

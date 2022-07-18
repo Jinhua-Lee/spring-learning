d11bfe4bf052c9bf24aed9a9c20a8da02c99177f
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-07-18 17:08:53 +0800

    [feature-tx] a. 事务隔离级别，补充原来的调试sql.

---
+++ src/main/resources/sql/debug/op_tx.sql
@@ -0,0 +1,11 @@
+-- 1. 关闭自动提交
+set @@autocommit = 0;
+
+-- 2. 开启事务
+start transaction;
+-- 2.1 操作
+update account set balance = balance - 3000 where id = 7;
+update account set balance = balance + 3000 where id = 1;
+-- 2.2 此处通过另一个会话，查询中间结果
+-- 2.3 回滚
+rollback;

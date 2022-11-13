28adb0b9097e13b324b2378d7c9df21c01de8849
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-11-13 19:06:53 +0800

    [docs] CgLib代理的一些梳理.

--- src/test/java/cn/spring/learning/PlainTest.java
+++ src/test/java/cn/spring/learning/PlainTest.java
@@ -2,15 +2,28 @@ package cn.spring.learning;
 
 import cn.spring.learning.beans.aop.bean.OriginFunctionImpl;
 import cn.spring.learning.beans.aop.bean.TargetFunction;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * @author Jinhua
  * @version 1.0
  * @date 2022/10/25 17:35
  */
-public class PlainTest {
+public class PlainTest implements Serializable {
 
     public static void main(String[] args) {
         System.out.println(TargetFunction.class.isAssignableFrom(OriginFunctionImpl.class));
+    }
+
+    @Test
+    @DisplayName(value = "通过class的getInterfaces方法，判断某个类型实现的接口")
+    public void testInterfaces() {
+        Class<?>[] interfaces = PlainTest.class.getInterfaces();
+        // 预期：Serializable接口
+        Arrays.stream(interfaces).forEach(System.out::println);
     }
 }

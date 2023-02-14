5eff2ddd3d95403b46dab46f405799711f6be6be
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-02-15 00:30:32 +0800

    [feat] rabbitmq配置参数调整.

--- src/main/java/cn/spring/learning/mq/producer/RabbitProducer.java
+++ src/main/java/cn/spring/learning/mq/producer/RabbitProducer.java
@@ -1,10 +1,10 @@
 package cn.spring.learning.mq.producer;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.time.Instant;
 
 /**
@@ -24,7 +24,7 @@ public class RabbitProducer {
         );
     }
 
-    @Resource
+    @Autowired
     public void setRabbitTemplate(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }

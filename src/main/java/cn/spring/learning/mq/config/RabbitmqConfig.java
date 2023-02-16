8cfb63862ec1782a756f7a5991c6477a76f7ed46
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-02-16 15:03:05 +0800

    [fix] 移除多余的队列配置.

--- src/main/java/cn/spring/learning/mq/config/RabbitmqConfig.java
+++ src/main/java/cn/spring/learning/mq/config/RabbitmqConfig.java
@@ -2,8 +2,6 @@ package cn.spring.learning.mq.config;
 
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.Data;
-import org.springframework.amqp.core.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,40 +28,5 @@ public class RabbitmqConfig {
         factory.setPassword(this.password);
         factory.setVirtualHost(virtualHost);
         return factory;
-    }
-
-    @Bean
-    public TopicExchange exA(@Value(value = "topicExA") String exchangeNameA) {
-        return new TopicExchange(exchangeNameA, true, false);
-    }
-
-    @Bean
-    public Queue queueA(@Value(value = "queueA") String queueNameA) {
-        return new Queue(queueNameA, true, false, false);
-    }
-
-    @Bean
-    public Queue queueB(@Value(value = "queueB")String queueNameB) {
-        return new Queue(queueNameB, true, false, false);
-    }
-
-    @Bean
-    public FanoutExchange exAb(@Value(value = "fanExAb") String fanoutExchangeName) {
-        return new FanoutExchange(fanoutExchangeName, true, false);
-    }
-
-    @Bean
-    public Binding oriBinding(Queue queueA, TopicExchange exA, @Value(value = "rkA") String oriRoutingKey) {
-        return BindingBuilder.bind(queueA).to(exA).with(oriRoutingKey);
-    }
-
-    @Bean
-    public Binding transFromBinding(Queue queueA, FanoutExchange exAb) {
-        return BindingBuilder.bind(queueA).to(exAb);
-    }
-
-    @Bean
-    public Binding transToBinding(Queue queueB, FanoutExchange exAb) {
-        return BindingBuilder.bind(queueB).to(exAb);
     }
 }
